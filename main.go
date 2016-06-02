f246f45ef5398ce1071fc2c49272882a4ae6880c
Seth Shelnutt <Shelnutt2@gmail.com>
2016-06-02 08:17:50 -0400

    Add support for getting username and password from commandline

--- main.go
+++ main.go
@@ -8,36 +8,57 @@ import (
 
 	goopt "github.com/droundy/goopt"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/howeyc/gopass"
 )
 
 var mariadbHost = os.Getenv("MYSQL_HOST")
 var mariadbHostPassed = goopt.String([]string{"-H", "--host"}, "", "Host to check mariadb status of")
-var mariadbPort = goopt.Int([]string{"-p", "--mysql_port"}, 3306, "Specify a port to connect to")
+var mariadbPort = goopt.Int([]string{"--mysql_port"}, 3306, "Specify a port to connect to")
 var mariadbTable = goopt.String([]string{"-t", "--table"}, "", "Table to build struct from")
-var mariadbDatabase = goopt.String([]string{"-d", "--database"}, "", "Database to for connection")
-var mysqlUser = os.Getenv("MYSQL_USERNAME")
-var mysqlPassword = os.Getenv("MYSQL_PASSWORD")
+var mariadbDatabase = goopt.String([]string{"-d", "--database"}, "nil", "Database to for connection")
+var mariadbPassword *string
+var mariadbUser = goopt.String([]string{"-u", "--user"}, "user", "user to connect to database")
 var verbose = goopt.Flag([]string{"-v", "--verbose"}, []string{}, "Enable verbose output", "")
 var packageName = goopt.String([]string{"--package"}, "", "name to set for package")
 var structName = goopt.String([]string{"--struct"}, "", "name to set for struct")
 
 func init() {
-	//Parse options
-	goopt.Parse(nil)
+	goopt.OptArg([]string{"-p", "--password"}, "", "Mysql password", getMariadbPassword)
+	//goopt.ReqArg([]string{"-u", "--user"}, "user", "user to connect to database", setUser)
 
 	// Setup goopts
 	goopt.Description = func() string {
 		return "Mariadb http Check"
 	}
-	goopt.Version = "0.0.1"
-	goopt.Summary = "mysql-to-struct [-H] [-p] [-v]"
+	goopt.Version = "0.0.2"
+	goopt.Summary = "mysql-to-struct [-H] [-p] [-v] --package pkgName --struct structName --database databaseName --table tableName"
+
+	//Parse options
+	goopt.Parse(nil)
 
 }
 
 func main() {
+
+	if mariadbUser == nil || *mariadbUser == "user" {
+		fmt.Println("Username is required! Add it with --user=name")
+		return
+	}
 	if mariadbHostPassed != nil && *mariadbHostPassed != "" {
 		mariadbHost = *mariadbHostPassed
 	}
+
+	if mariadbPassword != nil && *mariadbPassword == "" {
+		fmt.Print("Password: ")
+		pass, err := gopass.GetPasswd()
+		stringPass := string(pass)
+		mariadbPassword = &stringPass
+		if err != nil {
+			fmt.Println("Error reading password: " + err.Error())
+			return
+		}
+	}
+
 	if *verbose {
 		fmt.Println("Connecting to mysql server " + mariadbHost + ":" + strconv.Itoa(*mariadbPort))
 	}
@@ -52,7 +73,13 @@ func main() {
 		return
 	}
 
-	db, err := sql.Open("mysql", mysqlUser+":"+mysqlPassword+"@tcp("+mariadbHost+":"+strconv.Itoa(*mariadbPort)+")/"+*mariadbDatabase+"?&parseTime=True")
+	var err error
+	var db *sql.DB
+	if mariadbPassword != nil {
+		db, err = sql.Open("mysql", *mariadbUser+":"+*mariadbPassword+"@tcp("+mariadbHost+":"+strconv.Itoa(*mariadbPort)+")/"+*mariadbDatabase+"?&parseTime=True")
+	} else {
+		db, err = sql.Open("mysql", *mariadbUser+"@tcp("+mariadbHost+":"+strconv.Itoa(*mariadbPort)+")/"+*mariadbDatabase+"?&parseTime=True")
+	}
 	defer db.Close()
 
 	if err != nil {
@@ -92,4 +119,10 @@ func main() {
 
 	fmt.Println(string(struc))
 
+}
+
+func getMariadbPassword(password string) error {
+	mariadbPassword = new(string)
+	*mariadbPassword = password
+	return nil
 }
