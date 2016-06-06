4a531a0aa8e8acd9cb0a32c9c859d47e7cecbecc
Seth Shelnutt <Shelnutt2@gmail.com>
2016-06-06 19:42:11 -0400

    Add tests for additional mysql datatypes. #1

--- utils_test.go
+++ utils_test.go
@@ -6,16 +6,102 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-func TestGenerate(t *testing.T) {
+func TestMysqlStringGenerate(t *testing.T) {
 	expectedStruct :=
 		`package test
 
 type testStruct struct {
-	StringColumn string
+	NullStringColumn sql.NullString
+	StringColumn     string
 }
 `
 
-	columnMap := map[string]map[string]string{"stringColumn": {"nullable": "NO", "value": "varchar"}}
+	columnMap := map[string]map[string]string{
+		"stringColumn":     {"nullable": "NO", "value": "varchar"},
+		"nullStringColumn": {"nullable": "YES", "value": "varchar"},
+	}
+	bytes, err := Generate(columnMap, "testStruct", "test", false, false)
+
+	Convey("Should be able to generate map from string column", t, func() {
+		So(err, ShouldBeNil)
+		So(string(bytes), ShouldEqual, expectedStruct)
+	})
+}
+
+func TestMysqlDateGenerate(t *testing.T) {
+	expectedStruct :=
+		`package test
+
+type testStruct struct {
+	DateColumn      time.Time
+	DateTimeColumn  time.Time
+	TimeColumn      time.Time
+	TimeStampColumn time.Time
+}
+`
+
+	columnMap := map[string]map[string]string{
+		"DateColumn":      {"nullable": "NO", "value": "date"},
+		"DateTimeColumn":  {"nullable": "NO", "value": "datetime"},
+		"TimeColumn":      {"nullable": "NO", "value": "time"},
+		"TimeStampColumn": {"nullable": "NO", "value": "timestamp"},
+	}
+	bytes, err := Generate(columnMap, "testStruct", "test", false, false)
+
+	Convey("Should be able to generate map from string column", t, func() {
+		So(err, ShouldBeNil)
+		So(string(bytes), ShouldEqual, expectedStruct)
+	})
+}
+
+func TestMysqlFloatGenerate(t *testing.T) {
+	expectedStruct :=
+		`package test
+
+type testStruct struct {
+	DecimalColumn     float64
+	DoubleColumn      float64
+	FloatColumn       float32
+	NullDecimalColumn sql.NullFloat64
+	NullDoubleColumn  sql.NullFloat64
+	NullFloatColumn   sql.NullFloat64
+}
+`
+
+	columnMap := map[string]map[string]string{
+		"floatColumn":       {"nullable": "NO", "value": "float"},
+		"nullFloatColumn":   {"nullable": "YES", "value": "float"},
+		"doubleColumn":      {"nullable": "NO", "value": "double"},
+		"nullDoubleColumn":  {"nullable": "YES", "value": "double"},
+		"decimalColumn":     {"nullable": "NO", "value": "decimal"},
+		"nullDecimalColumn": {"nullable": "YES", "value": "decimal"},
+	}
+	bytes, err := Generate(columnMap, "testStruct", "test", false, false)
+
+	Convey("Should be able to generate map from string column", t, func() {
+		So(err, ShouldBeNil)
+		So(string(bytes), ShouldEqual, expectedStruct)
+	})
+}
+
+func TestMysqlIntGenerate(t *testing.T) {
+	expectedStruct :=
+		`package test
+
+type testStruct struct {
+	BigIntColumn     int64
+	IntColumn        int
+	NullBigIntColumn sql.NullInt64
+	NullIntColumn    sql.NullInt64
+}
+`
+
+	columnMap := map[string]map[string]string{
+		"intColumn":        {"nullable": "NO", "value": "int"},
+		"nullIntColumn":    {"nullable": "YES", "value": "int"},
+		"bigIntColumn":     {"nullable": "NO", "value": "bigint"},
+		"nullBigIntColumn": {"nullable": "YES", "value": "bigint"},
+	}
 	bytes, err := Generate(columnMap, "testStruct", "test", false, false)
 
 	Convey("Should be able to generate map from string column", t, func() {
